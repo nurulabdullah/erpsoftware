0dd56129dbbeb3314018da74c95e55546c864e49
nurulabdullah <48907769+nurulabdullah@users.noreply.github.com>
2019-08-14 14:58:14 +0700

    Update random.js

--- random.js
+++ random.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*11)+ 1);
+var x=Math.floor((Math.random()*10)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2019/05/why-you-need-manufacturing-production.html"}
 if(x=="2"){xxx="/2019/05/free-manufacturing-software.html"}
